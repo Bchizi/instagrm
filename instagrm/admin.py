6def8ca7841c2dee25f5c35cb6aae471ce6a1017
Collins Muriuki <murerwacollins@gmail.com>
2019-11-16 14:52:54 +0300

    Add admin class

--- instagrm/admin.py
+++ instagrm/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from instagrm.models import Post, Comment
 
+class PostAdmin(admin.ModelAdmin):
+    filter_horizontal =('comments',)
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, admin_class=PostAdmin)
 admin.site.register(Comment)
