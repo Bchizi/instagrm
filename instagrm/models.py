d2291b02918090589108b2654abea0e224942ec7
Collins Muriuki <murerwacollins@gmail.com>
2019-11-16 11:54:23 +0300

    Create user profile model

--- instagrm/models.py
+++ instagrm/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User)
+    profile_pic = ImageField(blank=True, manual_crop="800x800")
